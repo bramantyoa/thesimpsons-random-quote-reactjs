03dfdf59a4d5fea1fb1633f89d2b652efa0922b1
Bramantyo Adhilaksono <bramantyoadhilaksono@gmail.com>
2019-01-17 10:34:17 +0700

    Add fetch new quote button
    Add button to fetch another random quote from API

--- src/components/Index/author.js
+++ src/components/Index/author.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
+const characterImgStyle = {
+    maxHeight: '300px',
+    width: 'auto',
+}
+
 class Author extends Component {
     render() {
         return (
             <div className="author-section">
-                <img src={this.props.data.image} alt={this.props.data.character}/>
+                <img style={characterImgStyle}
+                className="character-img" 
+                src={this.props.data.image} 
+                alt={this.props.data.character}/>
                 <h2 id="quote-author">{this.props.data.character}</h2>
             </div>
         )
--- src/screens/Index/index.js
+++ src/screens/Index/index.js
@@ -3,10 +3,14 @@ import axios from 'axios';
 import Quote from '../../components/Index/quote';
 import Author from '../../components/Index/author';
 import Footer from '../../components/Index/footer';
+import ButtonGroup from '../../components/Index/button-group';
 
 class ScreensIndex extends Component {
     constructor(props) {
         super(props);
+
+        this.fetchNewQuote = this.fetchNewQuote.bind(this);
+        this.handleFetchNew = this.handleFetchNew.bind(this);
 
         this.state = {
             quoteFetched: false,
@@ -15,6 +19,26 @@ class ScreensIndex extends Component {
     }
 
     componentDidMount() {
+        // var offlineData = {
+        //     "quote": "Why are you pleople avoiding me? Does my withered face remind you of the grim specter of death?",
+        //     "character": "Abe Simpson",
+        //     "image": "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FAbrahamSimpson.png?1497567511593",
+        //     "characterDirection": "Right"
+        // }
+        // this.setState({ quoteFetched: true, APIresponse: offlineData });
+
+        this.fetchNewQuote();
+    }
+
+    fetchNewQuote() {
+        // var offlineData = {
+        //     "quote": "Shut up, brain. I got friends now. I don't need you anymore.",
+        //     "character": "Lisa Simpson",
+        //     "image": "https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FLisaSimpson.png?1497567512083",
+        //     "characterDirection": "Right"
+        // }
+        // this.setState({ quoteFetched: true, APIresponse: offlineData });
+
         axios.get(`https://thesimpsonsquoteapi.glitch.me/quotes`)
             .then(response => {
                 const APIresponse = response.data[0];
@@ -22,17 +46,22 @@ class ScreensIndex extends Component {
             });
     }
 
+    handleFetchNew() {
+        this.setState({ quoteFetched: false });
+        this.fetchNewQuote();
+    }
+
     render() {
         if (this.state.quoteFetched) {
             return (
                 <div className="quote-container">
                     <div className="quote-main">
-                    <div className="quote-section">
-                        <Quote data={this.state.APIresponse} />
-                        
-                        <Author data={this.state.APIresponse} />
+                        <div className="quote-section">
+                            <Quote data={this.state.APIresponse} />
+                            <Author data={this.state.APIresponse} />
                         </div>
                     </div>
+                    <ButtonGroup onClick={this.handleFetchNew} />
                     <Footer />
                 </div>
             );
